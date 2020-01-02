8398a2484bd1d9154555aa106d25947a0b945a48
Josh Cai <yuanshi419@gmail.com>
2020-01-01 20:38:58 -0800

    Add a button for whether it's possible or not

--- src/twentyfour/core.cljs
+++ src/twentyfour/core.cljs
@@ -1,6 +1,7 @@
 (ns twentyfour.core
     (:require 
         [clojure.math.combinatorics :as combo]
+        [clojure.string :as str]
         [dommy.core :as dommy]))
 
 (enable-console-print!)
@@ -32,23 +33,23 @@
     (map vector (rest (first (seq perm))) (last (seq perm))))
 )
 
-(defn solve [nums target]
+(defn solve
   "Solves for 24 given a list of numbers in nums."
+  [nums target]
   (filter (fn [perm]
     ;; Since ClojureScript does not support ratios, we check that
     ;; the result is within a small epsilon of the target.
-    (< (abs (- target (operate-reduce perm))) 0.00001))
-    (generate-permutations nums)
-  )
-)
+            (< (abs (- target (operate-reduce perm))) 0.00001))
+          (generate-permutations nums)))
 
-;; Display expressions in human readable form
-(defn- pretty-print [num tup]
+(defn- pretty-print 
+  "Display expressions in human readable form."
+  [num tup]
   (if ((last tup) :reverse)
     (str \( ((last tup) :display) " " num " " (first tup) \))
-    (str \( ((last tup) :display) " "(first tup) " " num \))
+    (str \( ((last tup) :display) " " (first tup) " " num \))
+    )
   )
-)
 (defn- pretty-print-reduce [perm]
   (reduce pretty-print
     (first (first (seq perm)))
@@ -60,13 +61,25 @@
   (js/parseInt (dommy/value (dommy/sel1 id)))
  )
 
-(defn- solve-handler [e]
+(defn- solve-handler []
   (dommy/set-text! (dommy/sel1 :#answer) 
-    (clojure.string/join "\n" 
+    (str/join "\n" 
       (distinct (map pretty-print-reduce 
-                     (solve [(get-int :#a) 
-                             (get-int :#b) 
-                             (get-int :#c) 
+                     (solve [(get-int :#a)
+                             (get-int :#b)
+                             (get-int :#c)
                              (get-int :#d)] 24))))))
 
 (dommy/listen! (dommy/sel1 :#solve) :click solve-handler)
+
+(defn- possible-handler [_]
+  (dommy/set-text! (dommy/sel1 :#answer)
+                   (let [solution (solve [(get-int :#a)
+                                          (get-int :#b)
+                                          (get-int :#c)
+                                          (get-int :#d)] 24)]
+                     (if (empty? solution) 
+                       "Not possible." 
+                       "Possible."))))
+
+(dommy/listen! (dommy/sel1 :#possible) :click possible-handler)
